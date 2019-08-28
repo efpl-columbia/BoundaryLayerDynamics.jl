794747074736d0c0933afbd8ef32c9f8cc0453a9
Manuel F Schmid <m.schmid@civil.ubc.ca>
2019-08-27 18:23:05 -0700

    Measure timing of tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,19 @@
 using ChannelFlow, Test
-
-import LinearAlgebra, Random, MPI
+import LinearAlgebra, Random, MPI, TimerOutputs
 include("test_utils.jl")
 
-# shorthand for convenience, since we use a lot of unexported functions in tests
-CF = ChannelFlow
+const CF = ChannelFlow # shorthand for convenience, since we use a lot of unexported functions in tests
+const to = TimerOutputs.TimerOutput()
 
 println("Testing ChannelFlow.jl... (serial)")
 @testset "Direct Numerical Simulation" begin
-    @testset "Basics & Transform" begin include("transform_test.jl") end
-    @testset "Advection Term" begin include("advection_test.jl") end
-    @testset "Diffusion Term" begin include("diffusion_test.jl") end
-    @testset "Pressure Solver" begin include("pressure_solver_test.jl") end
-    @testset "File I/O" begin include("output_test.jl") end
-    @testset "Poiseuille Flow" begin include("integration_test.jl") end
+    TimerOutputs.@timeit to "transform test" @testset "Basics & Transform" begin include("transform_test.jl") end
+    TimerOutputs.@timeit to "advection test" @testset "Advection Term" begin include("advection_test.jl") end
+    TimerOutputs.@timeit to "diffusion test" @testset "Diffusion Term" begin include("diffusion_test.jl") end
+    TimerOutputs.@timeit to "pressure test" @testset "Pressure Solver" begin include("pressure_solver_test.jl") end
+    TimerOutputs.@timeit to "output test" @testset "File I/O" begin include("output_test.jl") end
+    TimerOutputs.@timeit to "integration test" @testset "Poiseuille Flow" begin include("integration_test.jl") end
+    show(to); println() # TimerOutput has no newline at the end
 end
 
-println("Testing ChannelFlow.jl... (parallel, n=4)")
-begin run_mpi_test("runtests_mpi.jl", 4) end
+run_mpi_test("runtests_mpi.jl", 4)
--- test/runtests_mpi.jl
+++ test/runtests_mpi.jl
@@ -1,18 +1,22 @@
 using ChannelFlow, Test
-import LinearAlgebra, Random, MPI
-
+import LinearAlgebra, Random, MPI, TimerOutputs
 include("test_utils.jl")
 
-CF = ChannelFlow
-MPI.Init()
+const CF = ChannelFlow
+const to = TimerOutputs.TimerOutput()
 
-@testset MPITestSet "Basic MPI Tests" begin
-    include("transform_test.jl")
-    include("advection_test.jl")
-    include("diffusion_test.jl")
-    include("pressure_solver_test.jl")
-    include("output_test.jl")
-    include("integration_test.jl")
+MPI.Init()
+const show_output = MPI.Comm_rank(MPI.COMM_WORLD) == 1
+
+show_output && println("Testing ChannelFlow.jl... (parallel, n=", MPI.Comm_size(MPI.COMM_WORLD), ")")
+@testset MPITestSet "Direct Numerical Simulation" begin
+    TimerOutputs.@timeit to "transform test" include("transform_test.jl")
+    TimerOutputs.@timeit to "advection test" include("advection_test.jl")
+    TimerOutputs.@timeit to "diffusion test" include("diffusion_test.jl")
+    TimerOutputs.@timeit to "pressure test" include("pressure_solver_test.jl")
+    TimerOutputs.@timeit to "output test" include("output_test.jl")
+    TimerOutputs.@timeit to "integration test" include("integration_test.jl")
+    show_output && (show(to); println())
 end
 
 MPI.Finalize()
