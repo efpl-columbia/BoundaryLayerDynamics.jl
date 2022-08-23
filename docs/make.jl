cab909b5c9c8191b28d8111837db130ae8deeaa4
Manuel F. Schmid <m.schmid@civil.ubc.ca>
2022-08-23 09:23:38 -0400

    Remove outdated documentation

--- docs/make.jl
+++
@@ -1,6 +0,0 @@
-using Documenter, ChannelFlow
-
-makedocs(sitename = "ChannelFlow.jl",
-         pages = ["Overview" => "index.md", "installation.md", "usage.md"],
-         format = Documenter.HTML(edit_link = nothing),
-        )
